298df54908b69847c68ad0c653582d63a3d91d0c
Mathews <mathewsrodrigues9@gmail.com>
2022-06-27 19:24:48 -0300

    adicionando projetos

---
+++ src/component/projects/Projetos.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import '..style.css'
+import Header from '../Header';
+import Footer from '../Footer';
+
+export default class Projetos extends Component {
+  render() {
+    return (
+      <>
+      <Header />
+      <div>Projetos</div>
+      <Footer />
+      </>
+    )
+  }
+}
