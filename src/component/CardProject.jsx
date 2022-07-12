9faf0c74daf89af04ac61e0d62f4c1f44fc50d11
Mathews <mathewsrodrigues9@gmail.com>
2022-07-12 17:58:43 -0300

    adicionando favicon

--- src/component/CardProject.jsx
+++ src/component/CardProject.jsx
@@ -7,12 +7,12 @@ export default class CardProject extends Component {
   render() {
     return (
       <div className="card-projects">
-        <Link to=""><img className="img-projects" src={Paletas} alt="Capa do Projeto" /></Link>
+        <Link to="" target="_blank"><img className="img-projects" src={Paletas} alt="Capa do Projeto" /></Link>
         <h3>Pixels Art</h3>
         <h4>
           Tecnologias utilizadas no projeto: DOM, JavaScript
         </h4>
-        <Link to=""><button className="button-project" type="button">Visualizar</button></Link>
+        <Link to="" target="_blank"><button className="button-project" type="button">Visualizar</button></Link>
       </div>
     )
   }
