335e6f4eb07f84ebd02572d2fa50476b4b379bf7
Mathews <mathewsrodrigues9@gmail.com>
2022-07-12 17:43:26 -0300

    refatorando parte 3

--- src/component/CardProject.jsx
+++ src/component/CardProject.jsx
@@ -1,9 +1,19 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom';
+import '../styles/cardprojects.css';
+import Paletas from '../images/paletas.png';
 
 export default class CardProject extends Component {
   render() {
     return (
-      <div>CardProject</div>
+      <div className="card-projects">
+        <Link to=""><img className="img-projects" src={Paletas} alt="Capa do Projeto" /></Link>
+        <h3>Pixels Art</h3>
+        <h4>
+          Tecnologias utilizadas no projeto: DOM, JavaScript
+        </h4>
+        <Link to=""><button className="button-project" type="button">Visualizar</button></Link>
+      </div>
     )
   }
 }
--- src/component/Projects.jsx
+++ src/component/Projects.jsx
@@ -1,31 +1,21 @@
 import React, { Component } from 'react'
+import CardProject from './CardProject';
 import '../styles/projects.css';
 
 export default class Projects extends Component {
   render() {
     return (
-      <section className="central-box" id="projects">
-
-<div className="projetos">
-  <div id="projetosOficiais">
-    <p className="p-projects"><strong>Projetos Oficias</strong></p>
-    <ul>
-      <li >- Projeto Pixels Art - Brincando com Pixels</li>
-      <li >- Projeto Shopping Cart - Carrinho de Compras</li>
-    </ul>
-  </div>
-
-  <div id="projetosExtras">
-    <p className="p-projects"><strong>Projetos Extras</strong></p>
-    <ul>
-      <li >- Jogo da Velha</li>
-      <li >- Relógio JavaScript</li>
-      <li >- Web Weather</li>
-    </ul>
-  </div>
-</div>
-
-</section>
+      <section className="projects" id="projects">
+          <h2>Projetos</h2>
+        <div className="projetos">
+          <CardProject />
+          <CardProject />
+          <CardProject />
+          <CardProject />
+          <CardProject />
+          <CardProject />
+        </div>
+      </section>
     )
   }
 }
